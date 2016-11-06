0298a7b87bc4fcdf647583d7c19c00f61268f9e1
kdumayao <katrinaumyao@gmail.com>
2016-11-07 01:27:18 +0800

    edited

--- Exercise5/edit_data.php
+++ Exercise5/edit_data.php
@@ -47,7 +47,7 @@ if(isset($_POST['btn-cancel']))
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
+<title>CRUD</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body>
