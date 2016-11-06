bcf55f09240d8ab4d7afb9b9a196681e557afb35
kdumayao <katrinaumyao@gmail.com>
2016-11-07 01:31:23 +0800

    modified

--- Exercise5/SignUp.php
+++ Exercise5/SignUp.php
@@ -18,7 +18,7 @@ if(isset($_POST['btn-save']))
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD</title>
+<title>CRUD Operations</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body>
